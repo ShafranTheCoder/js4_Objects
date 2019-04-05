bfdb63d422523315901b8d83a13190cd560c0298
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-04-05 22:00:20 +0300

    hw4

--- js/main.js
+++ js/main.js
@@ -1,60 +1,16 @@
-'use strict';
-let firstNumber = 10;
-let secondNumber = 5;
-let operationType = 2;
-let result;
-
-function calculating(firstNumber, secondNumber, operationType) {
-    switch (operationType) {
-        case "+":
-            result = firstNumber + secondNumber;
-            alert(result);
-            break;
-        case "-":
-            result = firstNumber - secondNumber;
-            alert(result);
-            break;
-        case "*":
-            result = firstNumber * secondNumber;
-            alert(result);
-            break;
-        case "/":
-            result = firstNumber / secondNumber;
-            alert(result);
-            break;
-        default:
-            alert("Error! Write only +-*/ values")
+"use strict";
+let newUser = {
+    firstName : "Dima",
+    secondName : "Bogutskii",
+    getLogin: function() {
+        alert (this.firstName[0] + this.secondName) ;
     }
+};
+
+function createNewUser(){
+    newUser.firstName = prompt("Write your first name");
+    newUser.secondName = prompt("Write your second name");
+
 }
-
-function inputAsking() {
-    firstNumber = prompt("write first number", `${firstNumber}`);
-    firstNumber = parseInt(firstNumber);
-
-    secondNumber = prompt("Write second number", `${secondNumber}`);
-    secondNumber = parseInt(secondNumber);
-
-    operationType = prompt("Write operation");
-}
-
-function inputChecking() {
-    while (true) {
-        if (Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) {
-            alert("You wrote correct value. All is fine.");
-            return;
-        } else {
-            alert("Write integer value");
-            inputAsking();
-        }
-    }
-}
-
-
-
-function main() {
-    inputAsking();
-    inputChecking();
-    calculating(firstNumber, secondNumber, operationType);
-}
-
-main();
+createNewUser();
+newUser.getLogin();
