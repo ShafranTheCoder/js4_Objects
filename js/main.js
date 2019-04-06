58762bda58611ee67843ff6980a6da672c229a99
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-04-06 13:42:42 +0300

    did hw4

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 "use strict";
-let newUser = {
-    firstName : "Dima",
-    secondName : "Bogutskii",
-    getLogin: function() {
-        alert (this.firstName[0] + this.secondName) ;
-    }
-};
 
 function createNewUser(){
+    let newUser = {
+        getLogin: function() {
+            return console.log(this.firstName[0].toLowerCase() + this.secondName.toLowerCase()) ;
+        }
+    };
     newUser.firstName = prompt("Write your first name");
     newUser.secondName = prompt("Write your second name");
-
+    newUser.getLogin();
+    return newUser;
 }
 createNewUser();
-newUser.getLogin();
